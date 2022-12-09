aec9ba5f661cb9a27b985a700e7f837ff4e8527b
caio nakai <kaioesperto@hotmail.com>
2022-12-09 23:57:53 +0000

    add topping component

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
-import ScoopOption from "./ScoopOption";
 import { Row } from "react-bootstrap";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+
   useEffect(() => {
+    const abortController = new AbortController();
     (async () => {
-      const response = await fetch(`http://localhost:3030/${optionType}`);
+      const response = await fetch(`http://localhost:3030/${optionType}`, {
+        signal: abortController.signal,
+      });
       const data = await response.json();
       setItems(data);
     })();
 
     return () => {
-      //   unsub
+      abortController.abort();
     };
   }, [optionType]);
 
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
