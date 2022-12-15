06caa3beff292764678f445e9cd5fcf542b4bd33
caio nakai <kaioesperto@hotmail.com>
2022-12-15 11:11:19 +0000

    add alert banner component

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -2,24 +2,34 @@ import React, { useEffect, useState } from "react";
 import { Row } from "react-bootstrap";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const abortController = new AbortController();
     (async () => {
-      const response = await fetch(`http://localhost:3030/${optionType}`, {
-        signal: abortController.signal,
-      });
-      const data = await response.json();
-      setItems(data);
+      try {
+        const response = await fetch(`http://localhost:3030/${optionType}`, {
+          signal: abortController.signal,
+        });
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        setError(true);
+      }
     })();
 
     return () => {
       abortController.abort();
     };
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
