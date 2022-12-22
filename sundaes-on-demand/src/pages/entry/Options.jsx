56a47f3102649bf157c6f93eb76544d80d5153f9
Caio Nakai <caio.nakai@fccn.pt>
2022-12-22 17:26:25 +0000

    test and code context for scoops subtotal

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -3,10 +3,14 @@ import { Row } from "react-bootstrap";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -33,6 +37,8 @@ const Options = ({ optionType }) => {
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
+
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
@@ -41,7 +47,16 @@ const Options = ({ optionType }) => {
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 };
 
 export default Options;
